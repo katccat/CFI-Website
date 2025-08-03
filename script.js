0b0ea03d1679ed25bc181eab6a032f600f084ee1
katccat <kharryappleknight@gmail.com>
2025-08-03 15:38:51 -0400

    wrapping up booking page

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const hamburger_menu = document.getElementById("hamburger");
 const nav_links = document.getElementById("nav-links");
 const reveal_contact_button = document.getElementById("reveal-contact");
-const contact_area_div = document.getElementById("contact-area");
-const is_portrait = window.matchMedia("(max-width: 1000px)");
+const contact_area_div = document.getElementById("contact-text");
+const is_portrait = window.matchMedia("(max-width: 1024px)");
+let was_desktop = true;
+
 function nav_menu_show() {
 	hamburger_menu.classList.add("active");
 	nav_links.classList.add("visible");
@@ -14,15 +16,22 @@ function nav_menu_hide() {
 window.onload = function() {
 	if (is_portrait.matches) {
 		nav_links.classList.add("box-shadow");
+		was_desktop = false;
 	}
 }
+
 window.addEventListener('resize', function() {
 	if (is_portrait.matches) {
 		nav_links.classList.add("box-shadow");
+		if (was_desktop) {
+			nav_menu_hide();
+		}
+		was_desktop = false;
 	}
 	else { // landscape
-		nav_menu_show(); // unhides nav links if hidden for desktop view
+		nav_menu_show(); // makes sure nav links are visible in desktop view
 		nav_links.classList.remove("box-shadow");
+		was_desktop = true;
 	}
 });
 hamburger_menu.addEventListener('click', function() {
@@ -44,14 +53,37 @@ function reveal_contact() {
 
 	// Inject into contact area
 	contact_area_div.innerHTML = `
-		<p><i>
+		<p>
 			Email: <a href="mailto:${email}" class="persistent-link uncolored-link">${email}</a><br>
 			Phone: ${phone}
-		</i></p>
+		</p>
 	`;
 	contact_area_div.classList.add('visible');
+	contact_area_div.classList.remove('placeholder-text');
 	this.classList.add("active");
 }
 
 if (reveal_contact_button && contact_area_div) 
 	reveal_contact_button.addEventListener("click", reveal_contact);
+
+function start_slide_show(target_element, image_directory, image_list, duration) {
+	let index = 0;
+
+	update_image();
+	setInterval(update_image, duration);
+
+	function update_image() {
+		target_element.classList.add('fade-out');
+
+		// Wait for fade-out to complete before changing image
+		setTimeout(() => {
+			target_element.src = image_directory + image_list[index];
+			index = (index + 1) % image_list.length;
+
+			// Wait for the image to load before fading in
+			target_element.onload = () => {
+				target_element.classList.remove('fade-out');
+			};
+		}, 300); // match this to the CSS transition duration
+	}
+}
